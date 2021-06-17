c3ff06d6db90810aa069e06ffec6db91f883c6d5
Patryk Najda <ptrknjd@gmail.com>
2021-06-17 08:16:49 +0200

    Remove kafka related code from HealthCheck

--- extensions/couchbase-client/runtime/src/main/java/io/quarkus/couchbase/client/health/CouchbaseHealthCheck.java
+++ extensions/couchbase-client/runtime/src/main/java/io/quarkus/couchbase/client/health/CouchbaseHealthCheck.java
@@ -1,57 +1,50 @@
 package io.quarkus.couchbase.client.health;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-
-import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.common.Node;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
 import org.eclipse.microprofile.health.Readiness;
 
-import io.smallrye.common.annotation.Identifier;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.enterprise.context.ApplicationScoped;
 
 @Readiness
 @ApplicationScoped
 public class CouchbaseHealthCheck implements HealthCheck {
 
-    @Inject
-    @Identifier("default-kafka-broker")
-    Map<String, Object> config;
+    // TODO: use: https://docs.couchbase.com/java-sdk/current/concept-docs/health-check.html#ping as a basis
 
-    private AdminClient client;
+//    @Inject
+//    @Identifier("default-kafka-broker")
+//    Map<String, Object> config;
+
+//    private AdminClient client;
 
     @PostConstruct
     void init() {
-        Map<String, Object> conf = new HashMap<>(config);
-        conf.put(AdminClientConfig.REQUEST_TIMEOUT_MS_CONFIG, "5000");
-        client = AdminClient.create(conf);
+//        Map<String, Object> conf = new HashMap<>(config);
+//        conf.put(AdminClientConfig.REQUEST_TIMEOUT_MS_CONFIG, "5000");
+//        client = AdminClient.create(conf);
     }
 
     @PreDestroy
     void stop() {
-        client.close();
+//        client.close();
     }
 
     @Override
     public HealthCheckResponse call() {
-        HealthCheckResponseBuilder builder = HealthCheckResponse.named("Kafka connection health check").up();
+        HealthCheckResponseBuilder builder = HealthCheckResponse.named("Couchbase connection health check").up();
         try {
-            StringBuilder nodes = new StringBuilder();
-            for (Node node : client.describeCluster().nodes().get()) {
-                if (nodes.length() > 0) {
-                    nodes.append(',');
-                }
-                nodes.append(node.host()).append(':').append(node.port());
-            }
-            return builder.withData("nodes", nodes.toString()).build();
+//            StringBuilder nodes = new StringBuilder();
+//            for (Node node : client.describeCluster().nodes().get()) {
+//                if (nodes.length() > 0) {
+//                    nodes.append(',');
+//                }
+//                nodes.append(node.host()).append(':').append(node.port());
+//            }
+            return builder.build();
         } catch (Exception e) {
             return builder.down().withData("reason", e.getMessage()).build();
         }
